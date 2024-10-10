0ae586916a2474aad53c9cd844985c27e96acf08
Emmanuel796501~ <ajayie993@gmail.com>
2024-10-10 12:39:54 +0100

    commit

---
+++ NumbersDates/Challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+while (true) {
+llk
+Console.WriteLine("Enter Date (MM/DD/YYYY)  ");
+Console.WriteLine("Enter 'exit' to quit code ");
+
+string userinput = Console.ReadLine();
+
+if(userinput == "exit"){
+    break;
+}
+
+DateTime b;
+ if(DateTime.TryParse(userinput, out b)){
+    DateTime curdate = DateTime.Now;
+    TimeSpan dateDifference = b - curdate;
+    if(dateDifference.TotalDays > 0 ){
+        Console.WriteLine($"There are {Math.Ceiling(dateDifference.TotalDays)} days remaining till we reach {b}.");
+    }
+    else if(dateDifference.TotalDays < 0 ){
+        Console.WriteLine($" There are {Math.Abs(Math.Floor(dateDifference.TotalDays)) } days until we reach {curdate}.");
+
+    }
+
+ }
+}
